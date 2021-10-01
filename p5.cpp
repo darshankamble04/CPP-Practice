8b2a0e009324619b51386b9027056ba5b46263a0
DARSHAN S KAMBLE <darshankamble7371@gmail.com>
2021-10-02 00:30:45 +0530

    functions

--- p5.cpp
+++ p5.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main(){
 
-    // int num ;
-    // int flag =0;
-    // cout<<"Enter a number :";
-    // cin>>num;
+    int num ;
+    int flag =0;
+    cout<<"Enter a number :";
+    cin>>num;
 
-    // for(int i = 2; i< sqrt(num); i++){
-    //     if(num%i == 0){
-    //         flag = 1;
-    //     }
-    // }
-    // if(flag == 1){
-    //     cout<<"Number is not Prime .";
-    // }else{
-    //     cout<<"Number is Prime .";
-    // }
+    for(int i = 2; i< sqrt(num); i++){
+        if(num%i == 0){
+            flag = 1;
+        }
+    }
+    if(flag == 1){
+        cout<<"Number is not Prime .";
+    }else{
+        cout<<"Number is Prime .";
+    }
 
     // FACTORIAL
     int n;
